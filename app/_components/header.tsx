452d6ad14f32d70150000a3e35b4d937364e8d8e
jhonatan-oliveiradev <jhonatan.oliveira931002@gmail.com>
2024-01-29 21:38:25 -0300

    feat: welcome msg

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -5,7 +5,7 @@ import { MenuIcon } from "lucide-react";
 
 const Header = () => {
 	return (
-		<Card>
+		<Card className="bg-[#141518]">
 			<CardContent className="p-5 justify-between items-center flex flex-row">
 				<Image src="/logo.png" alt="FSW Barber" height={22} width={120} />
 				<Button variant="outline" size="icon" className="h-8 w-8">

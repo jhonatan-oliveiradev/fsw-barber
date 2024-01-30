2b2377edc80da0e4a7adc6449450dd5aee90956c
jhonatan-oliveiradev <jhonatan.oliveira931002@gmail.com>
2024-01-29 21:25:08 -0300

    feat: add initial header component

--- components/header.tsx
+++ components/header.tsx
@@ -6,10 +6,10 @@ import { MenuIcon } from "lucide-react";
 const Header = () => {
 	return (
 		<Card>
-			<CardContent>
+			<CardContent className="p-5 justify-between items-center flex flex-row">
 				<Image src="/logo.png" alt="FSW Barber" height={22} width={120} />
-				<Button variant="outline" size="icon">
-					<MenuIcon />
+				<Button variant="outline" size="icon" className="h-8 w-8">
+					<MenuIcon size={16} />
 				</Button>
 			</CardContent>
 		</Card>

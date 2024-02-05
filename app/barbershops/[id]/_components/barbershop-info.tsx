4d4b1897920a66b02fb383bb75296b431ba8e5ef
jhonatan-oliveiradev <jhonatan.oliveira931002@gmail.com>
2024-02-05 13:29:46 -0300

    feat: add barbershop info component

---
+++ app/barbershops/[id]/_components/barbershop-info.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { Button } from "@/app/_components/ui/button";
+import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
+import Image from "next/image";
+import { Barbershop } from "@prisma/client";
+import { useRouter } from "next/navigation";
+
+interface BarbershopInfoProps {
+  barbershop: Barbershop;
+}
+
+const BarbershopInfo = ({ barbershop }: BarbershopInfoProps) => {
+  const router = useRouter();
+
+  const handleBackClick = () => {
+    router.back();
+  };
+
+  return (
+    <div>
+      <div className="relative h-[250px] w-full">
+        <Button
+          className="absolute left-4 top-4 z-50"
+          size="icon"
+          variant="outline"
+          onClick={handleBackClick}
+        >
+          <ChevronLeftIcon />
+        </Button>
+        <Button
+          className="absolute right-4 top-4 z-50"
+          size="icon"
+          variant="outline"
+        >
+          <MenuIcon />
+        </Button>
+        <Image
+          src={barbershop.imageUrl}
+          fill
+          alt={barbershop.name}
+          style={{ objectFit: "cover" }}
+          className="opacity-75"
+        />
+      </div>
+
+      <div className="border-b border-solid border-secondary px-5 pb-6 pt-3">
+        <h1 className="text-xl font-bold">{barbershop.name}</h1>
+        <div className="mt-2 flex items-center gap-1">
+          <MapPinIcon className="text-primary" size={16} />
+          <p className="text-sm text-gray-400">{barbershop.address}</p>
+        </div>
+        <div className="mt-2 flex items-center gap-1">
+          <StarIcon className="fill-primary text-primary" size={16} />
+          <p className="text-sm text-gray-400">5,0 (899 avaliações)</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BarbershopInfo;
